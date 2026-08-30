9c7d5ec755c1ac386fcf0630107703ad14c47b85
Yana <utrsach@gmail.com>
2026-08-30 20:24:10 +0300

    Add initial project implementation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('DOMContentLoaded', function() {
+  const footer = document.querySelector('.footer');
+
+  if (footer) {
+    footer.classList.add('footer--visible');
+  }
+});
